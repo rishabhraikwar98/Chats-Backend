8f11dd0def1e2cc532be72f3bb0ae04e7c8d5371
rishabhraikwar98 <rishabhraikwar77@gmail.com>
2024-06-11 17:38:53 +0530

    cors changes

--- app.js
+++ app.js
@@ -4,11 +4,7 @@ require("dotenv").config();
 require("./database/connect");
 const rootRouter = require("./routes/index")
 const app = express();
-const corsConfig = {
-  origin: process.env.BASE_URL,
-  credentials: true,
-};
-app.use(cors(corsConfig));
+app.use(cors());
 app.use(express.json());
 app.use("/api/v1",rootRouter)
 //testing

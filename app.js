94d367318ee177192de8c6da5f877c4e77d5a9b8
rishabhraikwar98 <rishabhraikwar77@gmail.com>
2024-06-28 14:32:41 +0530

    body parser fix

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 require("./database/connect");
+const bodyParser = require("body-parser")
 const rootRouter = require("./routes/index")
 const app = express();
 app.use(cors({
   origin:process.env.BASE_URL,
 }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 app.use("/api/v1",rootRouter)
 //testing
